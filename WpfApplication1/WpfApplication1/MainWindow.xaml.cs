380a93ca3ab610c89789ffc2d7c1bc29bd83bc09
Julien Rosen <jlrosen416@gmail.com>
2015-12-03 11:24:02 -0500

    first commit

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApplication1
+{
+    public partial class MainWindow
+    {
+        private RelayCommand _goToPage;
+        public ICommand CmdGoToPage => _goToPage ?? (_goToPage = new RelayCommand(GoToPage));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        public void GoToPage(object o)
+        {
+            LinkNavigator.Navigate(NavigationTarget, new Uri((string)o, UriKind.Relative));
+        }
+    }
+}
